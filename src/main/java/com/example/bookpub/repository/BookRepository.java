67ac41278b30f3216d70dc68114cb3b9ccdfe5af
Alexandru Burghelea <burghelea.alex@gmail.com>
2019-04-05 00:04:02 +0300

    added more classes

--- src/main/java/com/example/bookpub/repository/BookRepository.java
+++ src/main/java/com/example/bookpub/repository/BookRepository.java
@@ -1,13 +1,13 @@
 package com.example.bookpub.repository;
 
 import com.example.bookpub.entity.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
     Book findBookByIsbn(String isbn);
 
     List<Book> findAll();
